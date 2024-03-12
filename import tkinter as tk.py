6e7cb8b4166a738a6605f5c0d656365cc6328f5a
akhil55666 <127723890+akhil55666@users.noreply.github.com>
2024-03-12 12:17:47 +0530

    Add files via upload

---
+++ import tkinter as tk.py
@@ -0,0 +1,16 @@
+import tkinter as tk
+
+calculation = ""
+
+def add_to_calculation(symbol):
+    pass
+
+def evaluate_calculation():
+    pass
+
+def clear_field():
+    pass
+
+root = tk.Tk()
+root.geometry("300x275")
+root.main.loop()
